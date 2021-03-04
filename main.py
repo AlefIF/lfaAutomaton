bcba9496d8c10a2eddcb9162f1bfe170878c3b49
AlefIF <aleffaria99@gmail.com>
2021-03-03 22:00:42 -0300

    separando o automatoFD

--- main.py
+++ main.py
@@ -1,109 +1,5 @@
-class AutomatoFD:
-    def __init__(self, Alfabeto):
-        Alfabeto = str(Alfabeto)
-        self.estados = set()
-        self.alfabeto = Alfabeto
-        self.transicoes = dict()
-        self.inicial = None
-        self.finais = set()
-
-    def limpaAfd(self):
-        self.__deuErro = False
-        self.__estadoAtual = self.inicial
-
-    def criaEstado(self, id, inicial=False, final=False):
-        id = int(id)
-        if id in self.estados:
-            return False
-        self.estados = self.estados.union({id})
-        if inicial:
-            self.inicial = id
-        if final:
-            self.finais = self.finais.union({id})
-        return True
-
-    def criaTransicao(self, origem, destino, simbolo):
-        origem = int(origem)
-        destino = int(destino)
-        simbolo = str(simbolo)
-        if not origem in self.estados:
-            return False
-        if not destino in self.estados:
-            return False
-        if len(simbolo) != 1 or not simbolo in self.alfabeto:
-            return False
-        self.transicoes[(origem, simbolo)] = destino
-        return True
-
-    def mudaEstadoInicial(self, id):
-        if not id in self.estados:
-            return
-        self.inicial = id
-
-    def mudaEstadoFinal(self, id, final):
-        if not id in self.estados:
-            return
-        if final:
-            self.finais = self.finais.union({id})
-        else:
-            self.finais = self.finais.difference({id})
-
-    def move(self, cadeia):
-        for simbolo in cadeia:
-            if not simbolo in self.alfabeto:
-                self.__deuErro = True
-                break
-            if (self.__estadoAtual, simbolo) in self.transicoes.keys():
-                novoEstado = self.transicoes[(self.__estadoAtual, simbolo)]
-                self.__estadoAtual = novoEstado
-            else:
-                self.__deuErro = True
-                break
-        return self.__estadoAtual
-
-    def deuErro(self):
-        return self.__deuErro
-
-    def estadoAtual(self):
-        return self.__estadoAtual
-
-    def estadoFinal(self, id):
-        return id in self.finais
-
-    def __str__(self):
-        s = 'AFD(E, A, T, i, F): \n'
-        s += '   E = { '
-        for e in self.estados:
-            s += '{}, '.format(str(e))
-        s += '} \n'
-        s += '   A = { '
-        for a in self.alfabeto:
-            s += "'{}', ".format(a)
-        s += '} \n'
-        s += '   T = { '
-        for (e, a) in self.transicoes.keys():
-            d = self.transicoes[(e, a)]
-            s += "({}, '{}')-->{}, ".format(e, a, d)
-        s += '} \n'
-        s += '   i = {} \n'.format(self.inicial)
-        s += '   F = { '
-        for e in self.finais:
-            s += '{}, '.format(str(e))
-        s += '}'
-        return s
-
-    def minimiza(self):
-        print('===================Minimizacao===================')
-        #verificando se o automato tem apenas um estado inicial
-        #acho q n precisa
-
-        #verificando se o automato e completo
-        if len(self.alfabeto) * len(self.estados) == len(self.transicoes):
-            print("Automato Completo - Seguindo")
-        else:
-            print("O automato nao e completo, criando novos estados")
-
-
+from AutomatoFD import AutomatoFD
+from TesteAFDAceita import TesteAFDAceita
 
 
 def exemploAFDFormat():
@@ -112,25 +8,21 @@ def exemploAFDFormat():
     print('(1) Conter o alfabeto')
     print('(2) Quantidade de estados')
     print('(3) Estado inicial')
-    print('(4) Estados finais separados por espaco')
+    print('(4) Estados finais')
     print('(5 a N) Transicoes')
-    print('(N+1) ter o simbolo - para indicar o fim das transicoes ')
-    print('(N+2) A cadeia em sequencia e sem espaco ')
     print('====================================Exemplo====================================')
     print('ab')
     print('4')
     print('1')
     print('4')
-    print('1 2 a')
-    print('2 1 a')
-    print('3 4 a')
-    print('4 3 a')
-    print('1 3 b')
-    print('3 1 b')
-    print('2 4 b')
-    print('4 2 b')
-    print('-')
-    print('abbabaabbbbbba')
+    print('12a')
+    print('21a')
+    print('34a')
+    print('43a')
+    print('13b')
+    print('31b')
+    print('24b')
+    print('42b')
     print('==================================FIM Exemplo==================================')
     return 0
 
@@ -166,23 +58,13 @@ def lerAFDtxt(arquivo):
                 #o mesmo caso que ocorreu na criacao do alfabeto
                 afd.mudaEstadoFinal(int(y), True)
         elif i >= 4:
-            #enquanto o texto for diferente de -
-            #sera estanciado as transicoes
-            #muita gambiarra a frente
-            if argumento[0] == '-':
-                i = -2
-            else:
-                afd.criaTransicao(argumento[0], argumento[1], argumento[2])
-        elif i == -1:
-            #zerando a variavel text
-            text = ''
-            for g in argumento:
-                text += g
-            cadeia = text
+            afd.criaTransicao(argumento[0][0], argumento[0][1], argumento[0][2])
         i = i + 1
 
-    arquivoLeitura.close()
     print(afd)
+
+
+    cadeia= input('Digite a cadeia ')
     afd.limpaAfd()
     parada = afd.move(cadeia)
     if not afd.deuErro() and afd.estadoFinal(parada):
@@ -210,14 +92,10 @@ def salvarAFDtxt():
     ADFtext.writelines(input('Digite o(s) Estado(s) final(is) (separados por espaco): '))
     ADFtext.writelines('\n')
     x = int(input('Digite a quantidade de transicoes: '))
-    print('Digite o estado atual, o estado novo e o simbolo (exemplo: 1 2 a)')
+    print('Digite o estado atual, o estado novo e o simbolo (exemplo: 12a)')
     for i in range(x):
         ADFtext.writelines(input('T'+str(i)+': '))
         ADFtext.writelines('\n')
-    ADFtext.writelines('-')
-    ADFtext.writelines('\n')
-    ADFtext.writelines(input('Digite a cadeia (em sequencia e sem espaco): '))
-    ADFtext.writelines('\n')
     ADFtext.close()
     return 0
 
@@ -245,7 +123,7 @@ def copiarAFtxt():
 
 if __name__ == '__main__':
 
-    arquivo1 = "C:/Users/aleff/PycharmProjects/AFD/testeAFD.txt"
+    #arquivoAuxiliar = "C:/Users/aleff/Desktop/lfa/teste.txt"
 
     while True:
         print('====================================MENU DE OPCOES====================================')
@@ -329,7 +207,7 @@ if __name__ == '__main__':
             inputN = input('Digite a opcao desejada: ')
             print('=================================')
             if inputN == '1':
-                lerAFDtxt(arquivo1)
+                TesteAFDAceita()
         elif inputN == '0':
             print('Fechando')
             break
